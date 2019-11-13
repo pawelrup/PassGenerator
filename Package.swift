839022658f003406f82f936bb3378bd308f78784
Paweł Rup <pawelrup@lobocode.pl>
2019-11-13 20:02:39 +0100

    Add missing dependencies

--- Package.swift
+++ Package.swift
@@ -13,14 +13,16 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-nio.git", from: "2.10.0"),
+        .package(url: "https://github.com/weichsel/ZIPFoundation.git", .upToNextMajor(from: "0.9.9")),
+        .package(url: "https://github.com/vapor/open-crypto.git", from: "4.0.0-alpha.2"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "PassGenerator",
-            dependencies: []),
+            dependencies: ["NIO", "ZIPFoundation", "OpenCrypto"]),
         .testTarget(
             name: "PassGeneratorTests",
             dependencies: ["PassGenerator"]),
