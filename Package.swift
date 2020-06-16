2ace2d1a33b9030f2ddbc3945fb994cac49589cb
Paweł Rup <pawelrup@lobocode.pl>
2020-06-16 22:41:27 +0200

    Packages update

--- Package.swift
+++ Package.swift
@@ -4,9 +4,9 @@
 import PackageDescription
 
 let dependencies: [PackageDescription.Package.Dependency] = [
-    .package(url: "https://github.com/apple/swift-nio.git", .upToNextMajor(from: "2.17.0")),
+    .package(url: "https://github.com/apple/swift-nio.git", .upToNextMajor(from: "2.18.0")),
     .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", .upToNextMajor(from: "1.3.1")),
-    .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0")
+    .package(url: "https://github.com/apple/swift-log.git", from: "1.2.0")
 ]
 let targets: [Target] = [
     .target(name: "PassGenerator", dependencies: [
