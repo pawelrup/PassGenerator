392dac06e19129cb67de838db188cbc23ab92294
Paweł Rup <pawelrup@lobocode.pl>
2020-07-06 21:28:57 +0200

    Update dependencies

--- Package.swift
+++ Package.swift
@@ -4,9 +4,9 @@
 import PackageDescription
 
 let dependencies: [PackageDescription.Package.Dependency] = [
-    .package(url: "https://github.com/apple/swift-nio.git", .upToNextMajor(from: "2.18.0")),
-    .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", .upToNextMajor(from: "1.3.1")),
-    .package(url: "https://github.com/apple/swift-log.git", from: "1.2.0")
+    .package(url: "https://github.com/apple/swift-nio.git", .upToNextMinor(from: "2.19.0")),
+    .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", .upToNextMinor(from: "1.3.1")),
+    .package(url: "https://github.com/apple/swift-log.git", .upToNextMinor(from: "1.4.0"))
 ]
 let targets: [Target] = [
     .target(name: "PassGenerator", dependencies: [
@@ -19,4 +19,11 @@ let targets: [Target] = [
 let products: [Product] = [
     .library(name: "PassGenerator", targets: ["PassGenerator"])
 ]
-let package = Package(name: "PassGenerator", platforms: [.macOS(.v10_13)], products: products, dependencies: dependencies, targets: targets)
+let package = Package(
+	name: "PassGenerator",
+	platforms: [.macOS(.v10_13)],
+	products: products,
+	dependencies: dependencies,
+	targets: targets,
+	swiftLanguageVersions: [.v5]
+)
