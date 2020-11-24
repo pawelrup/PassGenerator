a5979bd7233e3bc252d0ef14680c3b343a49434c
Paweł Rup <pawelrup@lobocode.pl>
2020-11-24 10:56:31 +0100

    Update packages

--- Package.swift
+++ Package.swift
@@ -4,8 +4,8 @@
 import PackageDescription
 
 let dependencies: [PackageDescription.Package.Dependency] = [
-    .package(url: "https://github.com/apple/swift-nio.git", .upToNextMinor(from: "2.21.0")),
-    .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", .upToNextMinor(from: "1.3.1")),
+    .package(url: "https://github.com/apple/swift-nio.git", .upToNextMinor(from: "2.24.0")),
+    .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", .upToNextMinor(from: "1.3.6")),
     .package(url: "https://github.com/apple/swift-log.git", .upToNextMinor(from: "1.4.0"))
 ]
 let targets: [Target] = [
